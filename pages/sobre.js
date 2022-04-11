9f5128059bdc9b65ade5f8a38bf6a71dd23a78c3
unknown <willbeckcg@gmail.com>
2022-04-11 20:26:22 -0300

    sobre atualização

--- pages/sobre.js
+++ pages/sobre.js
@@ -11,21 +11,26 @@ function Home() {
             </Head>
             <Menu />
             
-            <body className='sobre'>
-            <div className='sobre'><br></br><br></br><br></br><br></br><br></br><br></br>
-                <h2>Quem Somos !!!</h2><br></br><br></br><br></br>
-                {/*<p>A DW Treinamentos é uma empresa jovem com o objetivo simples de levar ate seus clientes um serviço seguro com qualidade e excelência desde o primeiro contato.
-                Somos uma empresa q preza pela segurança agilidade e modernidade dos nossos produtos, <br/>Oferedemos todo o suporte necessário para que você, cliente ou parceiro tenha a melhor experiência conosco.
-                Oferecemos cursos regulamentados pela legislação atualizados periodicamente oque garante a qualidade e legitimidade do nosso repertório de cursos que vão desde cursos de reciclagem p motoristas do dia a dia ate o profissional de cargas pesadas vivas ou perigosas.
-                <br></br>Venha conhecer a D&W Treinamentos, aqui você encontra o curso que vc precisa.</p>*/}
+            <body className='sobre'><br></br><br></br><br></br><br></br><br></br>
+            <div className='sobre-text'>
 
+                <h2 className='sobre-text-1'>Quem Somos &rarr;</h2><br></br>        
 
-                <p>A DW Treinamentos é uma empresa jovem com o simples objetivo de levar até seus clientes um serviço seguro e com qualidade e excelência desde o primeiro contato.</p>
-                <p>Prezamos pela segurança, agilidade e modernidade de nossos produtos.</p>
-                <p>Oferecemos todo o suporte necessário para que você, cliente ou parceiro, tenha a melhor experiência conosco. 
-                <p>Nossos cursos são regulamentados segundo a legislação vigente e atualizados periodicamente, garantindo a qualidade e legitimidade de nosso repertório, que vai de reciclagem ao motorista comum até o profissional de cargas pesadas vivas ou perigosas.<br></br>
-                Venha conhecer a DW Treinamentos.</p>
-                <p></p> Aqui, você encontra o curso que precisa.</p>
+                <div className='sobre-border'><p>A DW Treinamentos é uma empresa jovem com o simples objetivo de levar até seus clientes um serviço seguro e com qualidade e excelência desde o primeiro contato.</p>
+                    <p>Prezamos pela segurança, agilidade e modernidade de nossos produtos. Oferecemos todo o suporte necessário para que você, cliente ou parceiro, tenha a melhor experiência conosco.</p>
+                    <p>Nossos cursos são regulamentados segundo a legislação vigente e atualizados periodicamente, garantindo a qualidade e legitimidade de nosso repertório, que vai de reciclagem ao motorista comum até o profissional de cargas pesadas vivas ou perigosas.</p>
+                    <p>Venha conhecer a DW Treinamentos. Aqui, você encontra o curso que precisa!!!</p><br></br><br></br>
+                </div><br></br>
+
+                 <h2 className='sobre-text-1'> Objetivos &rarr;</h2><br></br>
+
+                 <div className='sobre-border'>
+                    <p>- Especializar motoristas profissionais para o transporte de cargas e pessoas através de treinamento.</p>
+                    <p>- Proporcionar a estes o conhecimento de seus direitos e deveres, atualizá-los sobre as leis de trânsito e os objetivos das empresas.</p>
+                    <p>- Conscientizar e sensibilizar o profissional quanto a melhor maneira de conduzir o veículo com segurança, visando a preservação da integridade física do condutor e dos passageiros, da carga do veículo, da população vizinha ao trajeto e do meio ambiente.</p>
+                    <p>- Evitar, ou ao menos diminuir o número de acidentes no trânsito.</p>
+                </div>
+                
                 
             </div>    
             
